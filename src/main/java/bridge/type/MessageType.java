f8a8b32d9da2fb9e80aa18b836b136978d86f6db
hoyeoon <hoyeon.backend@gmail.com>
2022-11-21 16:58:29 +0900

    refactor: 필드명 수정

--- src/main/java/bridge/type/MessageType.java
+++ src/main/java/bridge/type/MessageType.java
@@ -4,7 +4,7 @@ public enum MessageType {
     GAME_START("다리 건너기 게임을 시작합니다."),
     BRIDGE_LENGTH_INPUT("다리의 길이를 입력해주세요."),
     MOVE_SPACE_INPUT("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    GAME_RETRY_INPUT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_COMMAND_INPUT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_END("최종 게임 결과"),
     GAME_SUCCESS_OR_NOT("게임 성공 여부: "),
     TOTAL_TRY_COUNT("총 시도한 횟수: ");
